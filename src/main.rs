3fa28786baa2079b2dac27c44db334264cf5e57c
Higor <hfashigor@gmail.com>
2025-07-22 14:35:56 -0300

    First test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::process::Command;
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let python_check = Command::new("python")
+        .arg("--version")
+        .output();
+
+    println!("Comming to match python check");
+    match python_check {
+        Ok(output) => {
+            let out = output.stdout;
+            let out = String::from_utf8(out);
+
+            match out {
+                Ok(message) => {
+                    println!("{message}");
+                },
+                Err(_) => {
+                    print!("Error while converting rust");
+                }
+            }
+        },
+        Err(_) => {
+            show_error_message("Python not foung", "Python not installed");
+        }
+    }
+}
+
+fn show_error_message(title: &str, message: &str) {
+    unsafe {
+        println!("{title}");
+        println!("{message}");
+    }
 }
